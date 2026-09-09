aa08059d01d6aca130993d4c3ce006f0a8e46213
alvesssjean <jeanpaulo370@gmail.com>
2026-09-09 18:32:28 -0300

    feat: criação do model Mensagem

--- messenger/models.py
+++ messenger/models.py
@@ -6,3 +6,20 @@ class Profile(models.Model):
     chave_publica_ecdh = models.TextField(blank=True)
     chave_registrada_em = models.DateTimeField(null=True)
     fingerprint_verificado = models.BooleanField(default=False)
+
+class Mensagem(models.Model):
+    ORIGEM_CHOICES = [
+        ('enviada', 'Enviada'),
+        ('recebida', 'Recebida')
+    ]
+    remetente = models.ForeignKey(User, on_delete=models.CASCADE, related_name="enviadas")
+    destinatario = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recebidas")
+    blob = models.BinaryField()
+    criado_em = models.DateTimeField()
+    recebido_em = models.DateTimeField(auto_now_add=True)
+    origem = models.CharField(max_length=10, choices=ORIGEM_CHOICES)
+    
+    class Meta:
+            indexes = [
+                models.Index(fields=['destinatario', '-recebido_em']),
+            ]
