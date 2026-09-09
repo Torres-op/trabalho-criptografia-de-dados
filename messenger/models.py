4c0840704e8dbd5e4cdeab9d6cf2718198ee8928
alvesssjean <jeanpaulo370@gmail.com>
2026-09-09 18:13:19 -0300

    feat: criação do model Perfil (extensão do User)

--- messenger/models.py
+++ messenger/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    chave_publica_ecdh = models.TextField(blank=True)
+    chave_registrada_em = models.DateTimeField(null=True)
+    fingerprint_verificado = models.BooleanField(default=False)
