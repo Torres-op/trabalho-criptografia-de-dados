d3e2d30aa7bfb71c5f15d19df925fe745b232c11
alvesssjean <jeanpaulo370@gmail.com>
2026-09-11 14:52:16 -0300

    feat: proteção das views internas (ÉPICO 2.4)

--- messenger/views.py
+++ messenger/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
+@login_required
 def compose(request):
     return render(request, "messenger/compose.html")
 
 
+@login_required
 def translator(request):
     return render(request, "messenger/translator.html")
