fa089e3e6cc7bf11b9b8216fa7fd5ce003a67c46
Anderson Felipe Dias Domingos <139730074+Felipe-dmgs@users.noreply.github.com>
2026-09-15 20:29:14 -0300

    fix/UrlEView

--- messenger/urls.py
+++ messenger/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from . import views
-from django.contrib.auth.decorators import login_required
 
+from . import views
 
 app_name = "messenger"
 
@@ -10,4 +9,6 @@ urlpatterns = [
     path("", views.compose, name="compose"),
     path("translator/", views.translator, name="translator"),
     path("api/messages/", views.message_list, name="message-list"),
+    path("login/", LoginView.as_view(template_name="messenger/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- messenger/views.py
+++ messenger/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import JsonResponse
